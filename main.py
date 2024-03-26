bb2109984e09fde3d52945414d5c96349174c01d
lauraelefebvre <158224587+lauraelefebvre@users.noreply.github.com>
2024-03-26 10:53:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-#Ines Maria
+#AI project
+#Applied AI Image Reconstruction Using a Genetic Algortihm
 
-#Laura
+import cv2 
+
+#global variables
+qualities = []
+numParentsMating = 3
+
+#mutation percentage
+mutationPercent = 2
+
+#chromosomes per population
+solPerPop = 8
+M = 10 #width
+N = 10 #height
+
+
+#Image Manipulation
+imageRequest = input("Enter your image: ")
+image = cv2.imread(imageRequest)
+
+windowName = 'image'
+cv2.imshow(windowName, image)
+cv2.waitKey(0)
