b57fcce4586c031ca2d53ab6562875113be9bbd9
lauraelefebvre <158224587+lauraelefebvre@users.noreply.github.com>
2024-04-13 15:28:04 -0700

    commit

--- main.py
+++ main.py
@@ -24,6 +24,25 @@ windowName = 'image'
 cv2.imshow(windowName, image)
 cv2.waitKey(0)
 
+face = cv2.imread('face.png')
+face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+
 def img2vector(img_arr):
   fv = np.reshape(a = img_arr, newshape = (functools.reduce(operator.mul img_arr.shape)))
   return fv
+
+#storing face vector in a variable
+faceVector = img2vector(face)/255
+
+#convert to a matrix
+def convert2img(chromo, imgShape):
+  imgArr = mp.reshape(a = chromo, newshape = imgShape)
+  return imgArr
+
+def initialPop(imgShape, nIndividuals = 8):
+  seed(1)
+  init_population = np.empty(shape = (nIndividuals, functools.reduce(operator.mul, imgShape)), dtype = np.uint8)
+  for indv_num in range(nIndividuals):
+    #randomly generating inital population chromosome gene values
+    init_population[indv_num, :] = randint(0, 2, M * N)
+  return init_population
