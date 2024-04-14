91be54ebf39f96777d560a887ee19542f792c569
Ines <125163056+issmaria@users.noreply.github.com>
2024-04-13 17:23:50 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,41 +2,52 @@
 #Applied AI Image Reconstruction Using a Genetic Algortihm
 
 import cv2 
+from random import randint, seed
+import functools
+import operator
+import numpy as np
 
-#global variables
+#GLOBAL VARIABLES
 qualities = []
 numParentsMating = 3
-
 #mutation percentage
 mutationPercent = 2
-
 #chromosomes per population
 solPerPop = 8
 M = 10 #width
 N = 10 #height
+mutation_probability = 0.1
+num_generations = 100
 
 
-#Image Manipulation
+#IMAGE MANIPULATION
+#Load the target image
 imageRequest = input("Enter your image: ")
 image = cv2.imread(imageRequest)
 
+#Display the target image
 windowName = 'image'
 cv2.imshow(windowName, image)
 cv2.waitKey(0)
 
+#Load the image
 face = cv2.imread('face.png')
 face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
 
+#Converting the 2D image array to a 1D vector
 def img2vector(img_arr):
-  fv = np.reshape(a = img_arr, newshape = (functools.reduce(operator.mul img_arr.shape)))
-  return fv
+    #fv = [val for sublist in img_arr for val in sublist]
+    fv = np.reshape(a = img_arr, newshape = (functools.reduce(operator.mul, img_arr.shape)))
+    return fv
 
-#storing face vector in a variable
-faceVector = img2vector(face)/255
+#Storing face vector in a variable
+faceVector = img2vector(face)/255 #0-black, 255-white
 
-#convert to a matrix
+#Converting the vector to the image
+#Convert to a matrix
 def convert2img(chromo, imgShape):
-  imgArr = mp.reshape(a = chromo, newshape = imgShape)
+  #imgArr = [chromo[i:i+M] for i in range(0,len(chromo), M)]
+  imgArr = np.reshape(a = chromo, newshape = imgShape)
   return imgArr
 
 def initialPop(imgShape, nIndividuals = 8):
@@ -47,7 +58,33 @@ def initialPop(imgShape, nIndividuals = 8):
     init_population[indv_num, :] = randint(0, 2, M * N)
   return init_population
 
-#fitness function
+#fitness function - calculates the error between a target chromosome and an individual chromosome
 def fitness_fun(target_chrom, indv_chrom):
-  
-  return error
+    #calculates mean squared error as fitness
+    #square difference between each pair and pairs elements.
+    error = sum((x -y) **2 for x, y in zip(target_chrom, indv_chrom))/ len(target_chrom)
+    return error
+
+#GENETIC ALGORITHM FUNCTION
+def genetic_algorithm(target_image, population):
+    for generation in range (num_generations):
+        
+        #SELECTION
+        fitness_scores = [fitness_fun(target_image, indv_chrom) for indv_chrom in population]
+        #Parents based on the fitness scores
+        parents = []
+        for i in range(numParentsMating):
+            #2 random indices/individuals in the population
+            parent_indices = [randint(0, len(population) -1 )for i in range(2)]
+            parent1 = population[parent_indices[0]]
+            parent2 = population[parent_indices[1]]
+            #comparisson between the scores of the 2 prents. Finds the best pair.
+            if fitness_scores[parent_indices[0]] < fitness_scores[parent_indices[1]]:
+                parents.append(parent1)
+            else:
+                parents.append(parent2)
+                
+        
+        #CROSSOVER
+        
+        #MUTATION
