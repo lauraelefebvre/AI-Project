13127705d1b6fef31cd60a2159b867b2e5329b3b
lauraelefebvre <lauraelefebvre@gmail.com>
2024-04-19 15:25:12 -0700

    took out cv2

--- main.py
+++ main.py
@@ -174,14 +174,15 @@ population = genetic_algorithm(faceVector, population)
 #Display the result
 for indvChromo in population:
   reconstructImg = convert2img(indvChromo, (M, N)).astype(np.uint8)
+  plt.imshow(cv2.cvtColor(reconstructImg, cv2.COLOR_BGR2RGB))
   reconstructImgResizing = cv2.resize(reconstructImg, None, fx=5,fy=5, interpolation=cv2.INTER_LINEAR)
   plt.figure(figsize=(5,5))
-  plt.imshow(cv2.cvtColor(reconstructImgResizing, cv2.COLOR_BGR2RGB))
+  #plt.imshow(cv2.cvtColor(reconstructImgResizing, cv2.COLOR_BGR2RGB))
   
-cv2.namedWindow("Reconstructed Image", cv2.WINDOW_NORMAL)
-cv2.resizeWindow("Reconstructed Image", 500,500)
-cv2.imshow("Reconstructed Image", reconstructImg)
-cv2.waitKey(0)
+# cv2.namedWindow("Reconstructed Image", cv2.WINDOW_NORMAL)
+# cv2.resizeWindow("Reconstructed Image", 500,500)
+# cv2.imshow("Reconstructed Image", reconstructImg)
+# cv2.waitKey(0)
 
 
 #TESTING
